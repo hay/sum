2a3b2df9d1f8535091268e82101ec53751583933
Hay Kranen <huskyr@gmail.com>
2014-12-09 17:36:41 +0100

    Fixing lots of data errors, and added language support

--- web/lib/class-article.php
+++ web/lib/class-article.php
@@ -3,12 +3,37 @@ use \Httpful\Request;
 
 class Article extends Page  {
     const WIKIPEDIA_ENDPOINT = "%s/wikipedia/article?q=%s&lang=%s";
+    const WIKIDATA_ENDPOINT = "%s/wikidata/entity?q=%s&lang=%s";
 
     function __construct($qid) {
         parent::__construct();
         $this->fullurl = sprintf("%s/article/%s", $this->root, $qid);
 
-        $url = sprintf(self::WIKIPEDIA_ENDPOINT, API_ENDPOINT, urlencode($qid), $this->lang);
+        // First resolve the id to Wikipedia articles
+        $url = sprintf(self::WIKIDATA_ENDPOINT, API_ENDPOINT, $qid, $this->lang);
+        $res = Request::get($url)->send();
+
+        if (!$res->body->response) {
+            throw new Exception("No article available for this id");
+        }
+
+        $response = reset($res->body->response);
+
+        if (!property_exists($response->sitelinks, $this->lang)) {
+            throw new Exception("No article available in your language for this id");
+        }
+
+        if (property_exists($response, "labels")) {
+            $this->label = $response->labels;
+        }
+
+        if (property_exists($response, "descriptions")) {
+            $this->description = $response->descriptions;
+        }
+
+        $title = $response->sitelinks->{$this->lang}->title;
+
+        $url = sprintf(self::WIKIPEDIA_ENDPOINT, API_ENDPOINT, urlencode($title), $this->lang);
         $res = Request::get($url)->send();
 
         if (!$res->body->response) {
@@ -24,11 +49,8 @@ class Article extends Page  {
         });
 
         $this->images = array_values($this->images);
-
-        $this->firstimage = $this->images[0];
-
+        $this->firstimage = !empty($this->images) ? $this->images[0] : false;
         $this->text = $item->text;
-
         $this->thumbnail = $item->thumbnail;
     }
 }
--- web/lib/class-page.php
+++ web/lib/class-page.php
@@ -3,6 +3,7 @@ use \Httpful\Request;
 
 class Page {
     public $root, $lang, $title, $fullurl;
+    private $langcodes;
 
     const WIKIDATA_QUERY_ENDPOINT = "%s/wikidata/query?q=%s&lang=%s&from=0&size=%s";
     const DEFAULT_THUMB = "//upload.wikimedia.org/wikipedia/commons/thumb/3/34/Art-portrait-collage_2.jpg/660px-Art-portrait-collage_2.jpg";
@@ -12,9 +13,22 @@ class Page {
         $this->title = "The Sum of All Paintings";
         $this->fullurl = $this->root;
         $this->thumb = self::DEFAULT_THUMB;
+        $this->langcodes = json_decode(file_get_contents(ABSPATH . '/data/langcodes.json'));
+        $this->lang = $this->getLanguage();
+    }
 
-        // We should probably make this an url parameter or something
-        $this->lang = DEFAULT_LANGUAGE;
+    private function getLanguage() {
+        if (empty($_GET['lang'])) {
+            return DEFAULT_LANGUAGE;
+        }
+
+        $lang = strtolower($_GET['lang']);
+
+        if (array_key_exists($lang, $this->langcodes)) {
+            return $lang;
+        } else {
+            return DEFAULT_LANGUAGE;
+        }
     }
 
     protected function getQuery($query, $size = 4) {
