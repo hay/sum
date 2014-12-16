5f2b519538041716a4fe263351b2961b29dcca08
Hay Kranen <huskyr@gmail.com>
2014-12-16 11:45:40 +0100

    Small CSS changes

--- web/lib/class-searchresult.php
+++ web/lib/class-searchresult.php
@@ -1,10 +1,12 @@
 <?php
 class SearchResult extends Page {
-    public $searchresults;
+    public $searchresults, $q;
 
     function __construct($q) {
         parent::__construct();
+        $this->q = $q;
         $searchresults = new WikidataSearch($q, $this->lang);
+        $this->isSearch = true;
         $this->searchresults = $searchresults->getResultData();
     }
 }
