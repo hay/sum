93bd094a762e2f22c6b3251aa514423052c1feea
Hay Kranen <huskyr@gmail.com>
2014-12-20 15:11:08 +0100

    Adding a search input clear button

--- web/js/app.js
+++ web/js/app.js
@@ -58,6 +58,27 @@
         });
     }
 
+    function searchclear() {
+        $('[data-action="clearinput"]').each(function() {
+            var $el = $(this);
+            var $target = $($el.data('target'));
+
+            function checkShow() {
+                $el.toggleClass('show', !!$target.val());
+            }
+
+            $el.on('click', function() {
+                $target.val('');
+                checkShow();
+            });
+
+            $target.on('keyup', checkShow);
+
+            checkShow();
+        })
+    }
+
     loadWorkImage();
     photoswipe();
+    searchclear();
 })();
